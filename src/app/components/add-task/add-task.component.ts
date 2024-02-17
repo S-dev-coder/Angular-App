530a5fd29952911f413596d000d9a14e3c2dcc76
s-dev-coder <sk7137389@gmail.com>
2024-02-17 11:15:40 +0530

    Forms Module & ngModel || Submit & Create Task: > - Added FormsModule import in app.module.ts - Addeded ngSubmit onSubmit in task.component.html - Implemented onSubmit method in task.component.ts to handle form submission - Added event binding onAddTask addTask  in task.component.html - Implemented addTask task Task method in task.component.ts to add task using task service - Implemented addTask task Task method in task.service.ts to make HTTP POST request to add task - Ensured proper flow and integration across app.module.ts, task.component.html, task.component.ts, and task.serv ice.ts files.

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component , Output,EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
   day!: string;
   reminder: boolean = false;
+
+  onSubmit() {
+   if(!this.text) {
+     alert('Please add a task');
+     return;
+   }
+
+   const newTask = {  
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+this.onAddTask.emit(newTask);
+    //@todo - emit event
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
+
+
